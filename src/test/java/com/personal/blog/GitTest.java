1426404e7c63deb4d574e6bba3216f99f283ae63
lxl18014607019 <1965880301@qq.com>
2022-09-26 03:02:54 +0800

    上传测试

---
+++ src/test/java/com/personal/blog/GitTest.java
@@ -0,0 +1,20 @@
+package com.personal.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: luxinlong
+ * @CreateTime: 2022-09-26  03:00
+ * @Description: TODO
+ */
+@SpringBootTest
+public class GitTest {
+
+    @Test
+    void test(){
+        System.out.println("上传测试");
+    }
+
+}
+
